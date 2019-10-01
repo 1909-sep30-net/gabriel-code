6bc0cfd251583779045da6ff38ad29c3b760fad7
yetinight <gang@ucsd.edu>
2019-10-01 11:52:39 -0500

    Moved HelloRevature folder into this repo
    added:
    	HelloRevature folder

---
+++ HelloRevature/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloRevature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int str = 33;
+            Console.WriteLine(Convert.ToChar(str));
+        }
+    }
+}
