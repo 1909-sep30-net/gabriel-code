dfe0587ed30131320e135d60832daffa612961ca
yetinight <gang@ucsd.edu>
2019-11-08 15:37:18 -0600

    list animation

---
+++ angular-testing/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {
+  trigger,state,style,animate,transition, stagger, query
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  animations:[
+    trigger('openClose', [
+      // state('open',style({
+      //   height: '200px',
+      //   opacity: 0.5,
+      //   backgroundColor: 'yellow'
+      // })),
+      // state('close', style({
+      //   height: '100px',
+      //   opacity: 1,
+      //   backgroundColor: 'green'
+      // })),
+      // transition('open<=>close',[
+      //   animate('2s ease-out')
+      // ]),
+      transition('* <=> *',[
+        query(':leave',[
+          stagger(50,[
+            style({ opacity: 1 }),
+            animate('1s 100ms ease-in-out', style({
+              height: '100px',
+              opacity: 0.5,
+              backgroundColor: 'green'
+            })),
+            animate('1s 100ms ease-in-out', style({
+              height: '0px',
+              opacity: 0,
+              backgroundColor: 'yellow'
+            })),
+          ])
+        ],{optional: true}),
+        query(':enter',[
+          style({ opacity: 0 }),
+          stagger(200,[
+            animate('1s 100ms ease-in-out', style({
+              height: '100px',
+              opacity: 1,
+              backgroundColor: 'green'
+            }))
+          ])
+        ],{optional: true})
+      ]),
+    ]),
+  ]
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    //this.openToggle();
+  }
+  isOpen: boolean = true;
+  heroes: Hero[] = [];
+  animState = "";
+
+  openToggle(){
+    this.isOpen = !this.isOpen;
+    if (this.isOpen){
+      this.animState = 'open';
+      this.heroes = HEROES;
+    } else {
+      this.heroes = [];
+      this.animState = 'close'
+    }
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
